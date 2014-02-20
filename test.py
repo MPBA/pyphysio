df10110458c624c12ac7c16784689ac6e3e6f4a8
Alebats <alebat25@yahoo.it>
2014-02-20 22:37:00 +0100

    Found missing argumentin pyhrv.py

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ from utility import *
 
 np.random.seed()
 
-t=np.arange(0,10*np.pi,0.01*np.pi)
-RRraw=np.random.uniform(500,1500,1000)+100*np.sin(t)
+t = np.arange(0, 10*np.pi, 0.01*np.pi)
+RRraw = np.random.uniform(500, 1500, 1000)+100*np.sin(t)
 
-RRseries=DataSeries(RRraw)
+RRseries = DataSeries(RRraw)
 
 TD_ind, TD_lab = RRAnalysis.TD_indexes(RRseries)
 POIN_ind, POIN_lab = RRAnalysis.POIN_indexes(RRseries)
--- utility.py
+++ utility.py
@@ -2,53 +2,53 @@ import numpy as np
 from scipy import interpolate, signal
 
 
-
-
-
 # supplementary methods
-def power(spec,freq,fmin,fmax):
+def power(spec, freq, fmin, fmax):
     #returns power in band
-    band = np.array([spec[i] for i in range(len(spec)) if freq[i] >= fmin and freq[i]<fmax])
-    powerinband = np.sum(band)/len(spec)
+    band = np.array([spec[i] for i in range(len(spec)) if freq[i] >= fmin and freq[i] < fmax])
+    powerinband = np.sum(band) / len(spec)
     return powerinband
 
 
 def InterpolateRR(RR, Finterp):
     # returns cubic spline interpolated array with sample rate = Finterp
-    step=1/Finterp
-    BT=np.cumsum(RR)
-    xmin=BT[0]
-    xmax=BT[-1]
-    BT = np.insert(BT,0,0)
-    BT=np.append(BT, BT[-1]+1)
-    RR = np.insert(RR,0,0)
-    RR=np.append(RR, RR[-1])
+    step = 1 / Finterp
+    BT = np.cumsum(RR)
+    xmin = BT[0]
+    xmax = BT[-1]
+    BT = np.insert(BT, 0, 0)
+    BT = np.append(BT, BT[-1] + 1)
+    RR = np.insert(RR, 0, 0)
+    RR = np.append(RR, RR[-1])
 
-    tck = interpolate.splrep(BT,RR)
-    BT_interp = np.arange(xmin,xmax,step)
-    RR_interp = interpolate.splev(BT_interp,  tck)
-    return RR_interp,  BT_interp
+    tck = interpolate.splrep(BT, RR)
+    BT_interp = np.arange(xmin, xmax, step)
+    RR_interp = interpolate.splev(BT_interp, tck)
+    return RR_interp, BT_interp
 
-def BuildTakensVector(RR,m):
+
+def BuildTakensVector(RR, m):
     #creo embedded matrix
     #righe = Uj
     N = len(RR)
-    numelem=N-m+1
-    RRExp = np.zeros([numelem,m])
+    numelem = N - m + 1
+    RRExp = np.zeros([numelem, m])
     for i in range(numelem):
-       RRExp[i,:]=RR[i:i+m]
+        RRExp[i, :] = RR[i:i + m]
     return RRExp
 
-def smoothTriangle(data,degree):
-    triangle=np.array(range(degree)+[degree]+range(degree)[::-1])+1
-    smoothed=data[0:degree]
-    for i in range(degree,np.size(data)-degree*2):
-        point=data[i:i+len(triangle)]*triangle
-        smoothed= np.append(smoothed, sum(point)/sum(triangle))
-    smoothed=np.insert(smoothed, -1,  data[-(degree*2):])
+
+def smoothTriangle(data, degree):
+    triangle = np.array(range(degree) + [degree] + range(degree)[::-1]) + 1
+    smoothed = data[0:degree]
+    for i in range(degree, np.size(data) - degree * 2):
+        point = data[i:i + len(triangle)] * triangle
+        smoothed = np.append(smoothed, sum(point) / sum(triangle))
+    smoothed = np.insert(smoothed, -1, data[-(degree * 2):])
     return smoothed
 
-def peakdet(v, delta, x = None):
+
+def peakdet(v, delta, x=None):
     maxtab = []
     mintab = []
 
@@ -81,13 +81,13 @@ def peakdet(v, delta, x = None):
             mnpos = x[i]
 
         if lookformax:
-            if this < mx-delta:
+            if this < mx - delta:
                 maxtab.append((mxpos, mx))
                 mn = this
                 mnpos = x[i]
                 lookformax = False
         else:
-            if this > mn+delta:
+            if this > mn + delta:
                 mintab.append((mnpos, mn))
                 mx = this
                 mxpos = x[i]
@@ -95,20 +95,22 @@ def peakdet(v, delta, x = None):
 
     return np.array(maxtab), np.array(mintab)
 
+
 def lowpass_filter(X, fs, Wp):
     nyq = 0.5 * fs
-    wp=Wp/nyq # pass band Hz
-    ws=wp+0.5
-    N, wn = buttord(wp, ws, 5, 30) # calcola ordine per il filtro e la finestra delle bande
-    [bFilt, aFilt] = butter(N, wn, btype='lowpass') # calcola coefficienti filtro
-    sig=filtfilt(bFilt,aFilt,X) # filtro il segnale BVP
+    wp = Wp / nyq  # pass band Hz
+    ws = wp + 0.5
+    N, wn = buttord(wp, ws, 5, 30)  # calcola ordine per il filtro e la finestra delle bande
+    [bFilt, aFilt] = butter(N, wn, btype='lowpass')  # calcola coefficienti filtro
+    sig = filtfilt(bFilt, aFilt, X)  # filtro il segnale BVP
     return sig
+
 
 def highpass_filter(X, fs, Wp):
     nyq = 0.5 * fs
-    wp=Wp/nyq # pass band Hz
-    ws=wp-0.01
-    N, wn = buttord(wp, ws, 5, 30) # calcola ordine per il filtro e la finestra delle bande
-    [bFilt, aFilt] = butter(N, wn, btype='highpass') # calcola coefficienti filtro
-    sig=filtfilt(bFilt,aFilt,X) # filtro il segnale BVP
+    wp = Wp / nyq # pass band Hz
+    ws = wp - 0.01
+    N, wn = buttord(wp, ws, 5, 30)  # calcola ordine per il filtro e la finestra delle bande
+    [bFilt, aFilt] = butter(N, wn, btype='highpass')  # calcola coefficienti filtro
+    sig = filtfilt(bFilt, aFilt, X)  # filtro il segnale BVP
     return sig
