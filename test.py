b48e4d2c7c3d27dc70883e93d1ded5493ffc4f73
Alebats <alebat25@yahoo.it>
2014-02-20 22:49:02 +0100

    Some debug, it works

--- test.py
+++ test.py
@@ -9,10 +9,17 @@ RRraw = np.random.uniform(500, 1500, 1000)+100*np.sin(t)
 
 RRseries = DataSeries(RRraw)
 
-TD_ind, TD_lab = RRAnalysis.TD_indexes(RRseries)
-POIN_ind, POIN_lab = RRAnalysis.POIN_indexes(RRseries)
-FD_ind, FD_lab = RRAnalysis.FD_indexes(RRseries, 1)
+print 'Starting analysis'
 
-print(TD_lab)
-print(FD_lab)
-print(POIN_lab)
+print '.TD'
+TD_ind, TD_lab = RRAnalysis.TD_indexes(RRseries)
+print TD_lab
+print TD_ind
+print '.POIN'
+POIN_ind, POIN_lab = RRAnalysis.POIN_indexes(RRseries)
+print POIN_lab
+print POIN_ind
+print '.FD'
+FD_ind, FD_lab = RRAnalysis.FD_indexes(RRseries, 1)
+print FD_lab
+print FD_ind
