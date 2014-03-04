385e476c223386ee41d42b78f7cfc1b3f6db5f6f
Alebats <alebat25@yahoo.it>
2014-03-04 19:41:39 +0100

    2014-03-04 Tested: works with IBI column

--- Files.py
+++ Files.py
@@ -1,21 +1,33 @@
 __author__ = 'AleB'
 
-
 from PyHRVSettings import PyHRVDefaultSettings as Sett
 import pandas as pd
 from DataSeries import DataSeries
 
 
-def load_rr_data_series(path, column=Sett.load_rr_column_names, sep=Sett.csv_separator, *args):
-    d = pd.read_csv(path, sep, *args)
+def load_rr_data_series(path, column=Sett.load_rr_column_names, sep=Sett.csv_separator):
+    """
+    For galaxy use loads an rrs. column from a csv file
+    @param path:
+    @param column:
+    @param sep:
+    @return:
+    """
+    d = pd.read_csv(path, sep)
     if column in d.columns:
         return DataSeries(d[column])
     else:
         raise KeyError("Colonna %s non presente nel file %s".format(column, path))
 
 
-def save_rr_data_series(data_series, path, *args):
-    assert isinstance(data_series, DataSeries)
-    # TODO: check "args"
-    data_series.to_csv(path, args)
+def save_rr_data_series(data_series, path, sep=Sett.csv_separator):
+    """
+    For gaqlaxy use saves the DataSeries (rr) to a csv file.
+    @param data_series:
+    @param path:
+    @param sep:
+    @return:
+    """
+    assert isinstance(data_series, pd.Series)
+    data_series.to_csv(path, sep=sep, header=True)
 
