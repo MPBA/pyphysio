4cdb1da254bbffef8b076652ffe63a377e323651
andrea <andrea@localhost.localdomain>
2014-03-04 23:46:53 +0100

    2014-03-04 Edited HRV Indexes

--- Indexes.py
+++ Indexes.py
@@ -9,11 +9,12 @@ from PyHRVSettings import PyHRVDefaultSettings as Sett
 
 # TODO: comments
 # 1. Sarà necessario rendere le classi degli indici da mettere in cache figlie anche di CacheableDataCalc
-#   in modo da poter chiamare ad es. in HRMean.calculate un self._value = 60/RRMean.get(..)
+#    in modo da poter chiamare ad es. in HRMean.calculate un self._value = 60/RRMean.get(..)
 # 2. Spostare i calcoli nel costruttore es. HRMean !NB: data non dovrebbe essere None
 # 3. Per far funzionare tutti i TD deve andare il punto 1.
 # 4. Implementare gli indici
 
+# sostituito nella classe Index il metodo value con calculate
 
 class DataAnalysis(object):
     pass
@@ -27,13 +28,13 @@ class Index(object):
     @property
     def calculated(self):
         """
-        Returns weather the index is alredy calculated and up-to-date
+        Returns weather the index is already calculated and up-to-date
         @return: Boolean
         """
         return not (self._value is None)
 
     @property
-    def value(self):
+    def calculate(self):
         return self._value
 
     def update(self, data):
@@ -73,9 +74,24 @@ class PowerInBand(FDIndex):
         self._fmin = fmin
         self._fmax = fmax
 
-    def calculate(self):
-        #TODO: calc. power in (self._fmin, self._fmax)
-        return self._value
+        freq, spec, Total=###
+
+        indexes = np.array([i for i in range(len(spec)) if freq[i] >= fmin and freq[i]<fmax])
+        freq_band = freq[indexes]
+        self._value = np.sum(freq_band)/len(freq_band)
+
+
+class PowerInBandNormal(FDIndex):
+    def __init__(self, fmin, fmax, data=None):
+        super(PowerInBand, self).__init__(data)
+        self._fmin = fmin
+        self._fmax = fmax
+
+        freq, spec, Total=###
+
+        indexes = np.array([i for i in range(len(spec)) if freq[i] >= fmin and freq[i]<fmax])
+        freq_band = freq[indexes]
+        self._value = (np.sum(freq_band)/len(freq_band))/Total
 
 
 class PeakInBand(FDIndex):
@@ -84,88 +100,76 @@ class PeakInBand(FDIndex):
         self._fmin = fmin
         self._fmax = fmax
 
-    def calculate(self):
-        #TODO: calc. peak in (self._fmin, self._fmax)
-        return self._value
+        freq, spec, Total=###
+
+        indexes = np.array([i for i in range(len(spec)) if freq[i] >= fmin and freq[i]<fmax])
+        freq_band = freq[indexes]
+        spec_band = spec[indexes]
+        self._value = freq_band[np.argmax(freq_band)]
 
 
+#############
+# TIME DOMAIN
+#############
 class RRMean(TDIndex):
     def __init__(self, data=None):
         super(TDIndex, self).__init__(data)
-
-    def calculate(self):
         self._value = np.mean(self._data)
-        return self._value
 
 
 class HRMean(TDIndex):
     def __init__(self, data=None):
         super(TDIndex, self).__init__(data)
 
-    def calculate(self):
-        pass    # TODO: cacheable first
-                # è semplicemente 60/RRMean
-
+class RRmedian(TDIndex):
+    def __init__(self, data=None):
+        super(TDIndex, self).__init__(data)
+        self._value = np.median(self._data)
 
 class RRSTD(TDIndex):
     def __init__(self, data=None):
         super(TDIndex, self).__init__(data)
-
-    def calculate(self):
         self._value = np.std(self._data)
-        return self._value
 
 
 class HRSTD(TDIndex):
     def __init__(self, data=None):
         super(TDIndex, self).__init__(data)
 
-    # sovrascrivere il metodo calculate
-    def calculate(self):
-        pass    # TODO: cacheable first
-                # calcolabile da RRSTD
-
 
 class pNNx(TDIndex):
     def __init__(self, xthreshold, data=None):
         super(TDIndex, self).__init__(data)
         self._xth = xthreshold
-
-    def calculate(self):
         diff = RRDiff.get(self._data)
         self._value = 100.0 * sum(1 for x in diff if x > self._xth) / len(diff)
-        return self._value
 
 
 class NNx(TDIndex):
     def __init__(self, xthreshold, data=None):
         super(TDIndex, self).__init__(data)
         self._xth = xthreshold
-
-    def calculate(self):
         diff = RRDiff.get(self._data)
         self._value = 100.0 * sum(1 for x in diff if x > self._xth)
-        return self._value
 
 
 class RMSSD(TDIndex):
     def __init__(self, data=None):
         super(TDIndex, self).__init__(data)
-
-    # sovrascrivere il metodo calculate
-    def calculate(self):
-        pass
+        diff = RRDiff.get(self._data)
+        self._value = np.sqrt(sum(diff**2)/(len(diff)-1))
 
 
 class SDSD(TDIndex):
     def __init__(self, data=None):
         super(TDIndex, self).__init__(data)
-
-    # sovrascrivere il metodo calculate
-    def calculate(self):
-        pass
+        diff = RRDiff.get(self._data)
+        self._value = np.std(diff)
 
 
+#############
+# FREQ DOMAIN
+#############
 class VLF(PowerInBand):
     def __init__(self, data=None):
         super(VLF, self).__init__(Sett.bands_vlf_lower_bound, Sett.bands_vlf_upper_bound, data)
@@ -186,34 +190,65 @@ class Total(PowerInBand):
         super(Total, self).__init__(Sett.bands_vlf_lower_bound, Sett.bands_lf_upper_bound, data)
 
 
-# uses FFTCalc
-# should be Cacheable? need its value to calculate other indexes
+class VLFPeak(PeakInBand):
+    def __init__(self, data=None):
+        super(VLFPeak, self).__init__(Sett.bands_vlf_lower_bound, Sett.bands_vlf_upper_bound, data)
+
+
+class LFPeak(PeakInBand):
+    def __init__(self, data=None):
+        super(LFPeak, self).__init__(Sett.bands_vlf_upper_bound, Sett.bands_lf_upper_bound, data)
+
+
+class HFPeak(PeakInBand):
+    def __init__(self, data=None):
+        super(HFPeak, self).__init__(Sett.bands_lf_upper_bound, Sett.bands_hf_upper_bound, data)
+
+
+class VLFNormal(PowerInBandNormal):
+    def __init__(self, data=None):
+        super(VLFNormal, self).__init__(Sett.bands_vlf_lower_bound, Sett.bands_vlf_upper_bound, data)
+
+
+class LFNormal(PowerInBandNormal):
+    def __init__(self, data=None):
+        super(LFNormal, self).__init__(Sett.bands_vlf_upper_bound, Sett.bands_lf_upper_bound, data)
+
+
+class HFNormal(PowerInBandNormal):
+    def __init__(self, data=None):
+        super(HFNormal, self).__init__(Sett.bands_lf_upper_bound, Sett.bands_hf_upper_bound, data)
+
+
 class LFHF(FDIndex):
     def __init__(self, data=None):
         super(FDIndex, self).__init__(data)
-
-    # sovrascrivere il metodo calculate
-    def calculate(self):
-        pass
+        #LF/HF
 
 
-# calculate poincare' index
+class NormalLF(FDIndex):
+    def __init__(self, data=None):
+        super(FDIndex, self).__init__(data)
+        #LF/(LF+HF)
+
+
+class NormalHF(FDIndex):
+    def __init__(self, data=None):
+        super(FDIndex, self).__init__(data)
+        # HF/(LF+HF)
+
+
+#############
+# NLIN DOMAIN
+#############
 class PoinIndex(Index):
     def __init__(self, data=None):
         super(PoinIndex, self).__init__(data)
-
-    # sovrascrivere il metodo calculate
-    def calculate(self):
-        pass
 
 
 class NLIndex(Index):
     def __init__(self, data=None):
         super(NLIndex, self).__init__(data)
-
-    # sovrascrivere il metodo calculate
-    def calculate(self):
-        pass
 
 
 class RRAnalysis(DataAnalysis):
@@ -232,29 +267,6 @@ class RRAnalysis(DataAnalysis):
         return len(series.get_series)
 
     @staticmethod
-    def TD_indexes(series):
-        """ Returns TD indexes """
-        RR = series
-        RRmean = np.mean(RR)
-        RRSTD = np.std(RR)
-
-        RRDiffs = np.diff(RR)
-
-        RRDiffs50 = [x for x in np.abs(RRDiffs) if x > 50]
-        pNN50 = 100.0 * len(RRDiffs50) / len(RRDiffs)
-        RRDiffs25 = [x for x in np.abs(RRDiffs) if x > 25]
-        pNN25 = 100.0 * len(RRDiffs25) / len(RRDiffs)
-        RRDiffs10 = [x for x in np.abs(RRDiffs) if x > 10]
-        pNN10 = 100.0 * len(RRDiffs10) / len(RRDiffs)
-
-        RMSSD = np.sqrt(sum(RRDiffs ** 2) / (len(RRDiffs) - 1))
-        SDSD = np.std(RRDiffs)
-
-        labels = np.array(['RRMean', 'RRSTD', 'pNN50', 'pNN25', 'pNN10', 'RMSSD', 'SDSD'], dtype='S10')
-
-        return [RRmean, RRSTD, pNN50, pNN25, pNN10, RMSSD, SDSD], labels
-
-    @staticmethod
     def poin_indexes(series):
         """ Returns Poincare' indexes """
         RR = series
@@ -267,48 +279,6 @@ class RRAnalysis(DataAnalysis):
         labels = ['sd1', 'sd2', 'sd12', 'sEll']
 
         return [sd1, sd2, sd12, sEll], labels
-
-    @staticmethod
-    def FD_indexes(series, Finterp):
-
-
-        # freqs=np.arange(0, 2, 0.0001)
-        #
-        # # calculates AR coefficients
-        # AR, P, k = spct.arburg(RR_interp*1000, 16) #burg
-        #
-        # # estimates PSD from AR coefficients
-        # spec = spct.arma2psd(AR, T=0.25, NFFT=2*len(freqs))
-        freqs, spec = FFTCalc.get(series, Finterp, use_cache=True)
-
-        # calculates power in different bands
-        VLF = power(spec, freqs, 0, 0.04)
-        LF = power(spec, freqs, 0.04, 0.15)
-        HF = power(spec, freqs, 0.15, 0.4)
-        Total = power(spec, freqs, 0, 2)
-        LFHF = LF / HF
-        nVLF = VLF / Total
-        nLF = LF / Total
-        nHF = HF / Total
-
-        LFn = LF / (HF + LF)
-        HFn = HF / (HF + LF)
-        Power = [VLF, HF, LF]
-
-        Power_Ratio = Power / sum(Power)
-        # Power_Ratio=spec/sum(spec) # uncomment to calculate Spectral Entropy using all frequencies
-        Spectral_Entropy = 0
-        lenPower = 0 # tengo conto delle bande che ho utilizzato
-        for i in xrange(0, len(Power_Ratio)):
-            if Power_Ratio[i] > 0: # potrei avere VLF=0
-                Spectral_Entropy += Power_Ratio[i] * np.log(Power_Ratio[i])
-                lenPower += 1
-        Spectral_Entropy /= np.log(lenPower) #al posto di len(Power_Ratio) perche' magari non ho usato VLF
-
-        labels = np.array(['VLF', 'LF', 'HF', 'Total', 'nVLF', 'nLF', 'nHF', 'LFn', 'HFn', 'LFHF', 'SpecEn'],
-                          dtype='S10')
-
-        return [VLF, LF, HF, Total, nVLF, nLF, nHF, LFn, HFn, LFHF, Spectral_Entropy], labels
 
 
 class RRFilters(DataAnalysis):
@@ -327,5 +297,3 @@ class RRFilters(DataAnalysis):
         return series
 
         # TODO: add analysis scripts like in the example
-
-
