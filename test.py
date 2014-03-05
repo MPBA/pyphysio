842e5b21eb82e3eb92b1d58cfe6d731a54e257e5
andrea <a.bizzego@gmail.com>
2014-03-05 16:17:49 +0100

    2014-05-03

--- test.py
+++ test.py
@@ -16,17 +16,18 @@ def delay(v=True):
 def test(RRseries):
     print '1. TD'
     rrm, rrs, pnnx, nnx = In.RRMean(RRseries), In.RRSTD(RRseries), In.PNNx(50, RRseries), In.NNx(50, RRseries)
+    #vlf,lf,hf, lfhf, vlf_peak = In.VLF(RRseries), In.LF(RRseries), In.HF(RRseries), In.LFHF(RRseries), In.VLFPeak(RRseries)
     print "RRMean: ", rrm.value
     print "RRSTD: ", rrs.value
     print "PNNx: ", pnnx.value
     print "NNx: ", nnx.value
+    #print vlf,lf,hf, lfhf, vlf_peak
     delay()
 
 if __name__ == '__main__':
     delay(False)
 
-    RRseries = load_rr_data_series("/media/ale/44A0-BCA5/gx/Subject_data/B01.txt")
-    save_rr_data_series(RRseries, "/media/ale/44A0-BCA5/B01.txt")
+    RRseries = load_rr_data_series("A05.txt")
 
     print 'Starting analysis'
     test(RRseries)
