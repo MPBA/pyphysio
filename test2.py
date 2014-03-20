57c66079089350d495fff57f2b4b64b151088750
Alebats <alebat25@yahoo.it>
2014-03-20 15:45:31 +0100

    Added file extension

--- test2.py
+++ test2.py
@@ -2,4 +2,4 @@ from Galaxy.calculate_HRV_indexes import *
 
 hrv_list = np.ones(27)
 HRVAnalysis = GalaxyHRVAnalysis()
-hrv_data = HRVAnalysis.execute('A05', hrv_list)
+hrv_data = HRVAnalysis.execute('A05.txt', hrv_list)
