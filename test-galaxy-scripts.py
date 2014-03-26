a421d29352d46aa042ff8c1111bfcb14d85561ec
Alebats <alebat25@yahoo.it>
2014-03-26 14:33:51 +0100

    Doesn't load Galaxy

--- test-galaxy-scripts.py
+++ test-galaxy-scripts.py
@@ -1,9 +1,8 @@
 from Galaxy.calculate_HRV_indexes import *
 from Galaxy.load_RR_data import *
+import numpy as np
 
 hrv_list = np.ones(27)
-GalaxyLoadRR(input="A05.txt", output="A05.ibi")
-HRVAnalysis = GalaxyHRVAnalysis(input="A05.ibi", output="test2.txt", indexes=hrv_list)
-hrv_data = HRVAnalysis.execute()
+GalaxyLoadRR(input="A05.txt", output="A05.ibi").execute()
+print GalaxyHRVAnalysis(input="A05.ibi", output="test2.txt", indexes=hrv_list).execute()
 
-print hrv_data
