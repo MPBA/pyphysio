2838e9d7edd519f2b2388275522cdfb4a737e369
Alebats <alebat25@yahoo.it>
2014-03-26 16:31:42 +0100

    Updated

--- setup.py
+++ setup.py
@@ -35,9 +35,9 @@ setup(
         'Programming Language :: Python',
     ],
     requires=[
-        'pandas >= 0.13.1',
-        'numpy >= 1.7.1',
-        'scipy >= 0.12.0'
+        'pandas (>= 0.13.1)',
+        'numpy (>= 1.7.1)',
+        'scipy (>= 0.12.0)'
     ],
 
     package_dir={'pyHRV': 'pyHRV'},
