644c92e2cfef4c1a82316782b8a3f0389c84aded
Alebats <alebat25@yahoo.it>
2014-03-27 18:02:05 +0100

    Private

--- ver.py
+++
@@ -1,8 +0,0 @@
-vh = open('version', 'r')
-lines = vh.readlines()
-version = lines[-1].rstrip('\n').rstrip('\r').split('.')
-vh.close()
-vh = open('version', 'a')
-version[-1] = (int(version[-1]) + 1).__str__()
-vh.writelines("\n" + ".".join(version))
-vh.close()
