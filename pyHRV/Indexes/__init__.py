63bbe27dffdfe51f85505d6a0172ab255c1a4566
Alebats <alebat25@yahoo.it>
2014-03-27 17:15:36 +0100

    Added __all_indexes_ variable and getter

--- pyHRV/Indexes/__init__.py
+++ pyHRV/Indexes/__init__.py
@@ -5,7 +5,9 @@ import FDIndexes
 from FDIndexes import *
 import NonLinearIndexes
 from NonLinearIndexes import *
+__all_indexes__ = list()
+__all_indexes__.extend(TDIndexes.__all__)
+__all_indexes__.extend(FDIndexes.__all__)
+__all_indexes__.extend(NonLinearIndexes.__all__)
 __all__ = ['TDIndexes', 'FDIndexes', 'NonLinearIndexes']
-__all__.extend(TDIndexes.__all__)
-__all__.extend(FDIndexes.__all__)
-__all__.extend(NonLinearIndexes.__all__)
+__all__.extend(__all_indexes__)
--- pyHRV/__init__.py
+++ pyHRV/__init__.py
@@ -2,12 +2,21 @@ __author__ = "AleB"
 import Files
 import PyHRVSettings
 import Indexes
+import DataSeries as _Ds
+
 __all__ = ['Files', 'PyHRVSettings', 'Indexes']
-__all__.extend(DataSeries.__all__)
+__all__.extend(_Ds.__all__)
 __all__.extend(Files.__all__)
 __all__.extend(PyHRVSettings.__all__)
 __all__.extend(Indexes.__all__)
+
+
+def get_available_indexes():
+    return Indexes.__all_indexes__
+
+
 from DataSeries import *
 from Files import *
 from PyHRVSettings import *
 from Indexes import *
+del _Ds
