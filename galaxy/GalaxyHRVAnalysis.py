0221a7893ea3cda0766bd8b26c93b306e0d877fe
Alebats <alebat25@yahoo.it>
2014-04-08 16:38:56 +0200

    Extracted indexes computation method from Galaxy interface

--- galaxy/GalaxyHRVAnalysis.py
+++ galaxy/GalaxyHRVAnalysis.py
@@ -13,23 +13,28 @@ class GalaxyHRVAnalysis(ParamExecClass):
     kwargs['indexes'] --> indexes list [1,0, ... 1,0]
     """
 
-    def execute(self):
-        data = pyHRV.Files.load_rr(self._kwargs['input'])
-        indexes = self._kwargs['indexes']
+    @staticmethod
+    def calculate_indexes(data, indexes=None):
         values = dict()
         errors = list()
-
-        # Pre-parse the list to save time
+        if indexes is None:
+            indexes = pyHRV.get_available_indexes()
         for index in indexes:
             if not hasattr(pyHRV, index):
                 errors.append(index)
-
         if len(errors) > 0:
             raise NameError(pyHRV.PyHRVDefaultSettings.Local.names(
                 pyHRV.PyHRVDefaultSettings.Local.indexes_not_found, errors))
         else:
             for index in indexes:
                 values[index] = getattr(pyHRV, index)(data).value
+        return values
+
+    def execute(self):
+        data = pyHRV.Files.load_rr(self._kwargs['input'])
+        indexes = self._kwargs['indexes']
+
+        values = self.calculate_indexes(data, indexes)
 
         pyHRV.Files.save_data_series(pd.Series(values), self._kwargs['output'])
         return values
--- tests/test-offline-galaxy-scripts.py
+++ tests/test-offline-galaxy-scripts.py
@@ -3,7 +3,7 @@ from galaxy.GalaxyLoadRR import *
 from pyHRV import get_available_indexes
 
 hrv_list = get_available_indexes()
-in_file = "z_data/A05.txt"
+in_file = "../z_data/A05.txt"
 rr_file = "rr.ibi"
 out_file = "indexes.txt"
 GalaxyLoadRR(input=in_file, output=rr_file, column="IBI").execute()
