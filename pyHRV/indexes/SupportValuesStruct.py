43591c8b90d00de35eb09368273c980ef0ab17c9
Alebats <alebat25@yahoo.it>
2014-04-28 14:28:45 +0200

    Shifted SupportValuesStruct

---
+++ pyHRV/indexes/SupportValuesStruct.py
@@ -0,0 +1,74 @@
+__author__ = 'AleB'
+__all__ = ['SupportValues']
+
+
+class SupportValues(object):
+    def __init__(self, window=50):
+        self._last = []
+        self._win_size = window
+        self._p = {}
+        self._sum = 0
+        self._len = 0
+        self._old = None
+
+    def get(self, index, default=0):
+        if not index in self._p:
+            self._p[index] = default
+        return self._p[index]
+
+    def set(self, index, value):
+        self._p[index] = value
+
+    @property
+    def old(self):
+        return self._last[0]
+
+    @property
+    def last(self):
+        return self._last[0]
+
+    @property
+    def new(self):
+        return self._last[-1]
+
+    @property
+    def vec(self):
+        return self._last
+
+    def update(self, values):
+        for a in values:
+            self._enqueue(a)
+        if self._win_size >= 0:
+            while self.len > self._win_size:
+                self._dequeue()
+
+    @property
+    def len(self):
+        return self._len
+
+    @property
+    def sum(self):
+        return self._sum
+
+    @property
+    def min(self):
+        return self._sum
+
+    @property
+    def max(self):
+        return self._sum
+
+    @property
+    def ready(self):
+        return self._win_size < 0 < self.len or self.len == self._win_size
+
+    def _enqueue(self, val):
+        self._last.append(val)
+        self._sum += val
+
+    def _dequeue(self):
+        val = self._last[0]
+        del self._last[0]
+        self._sum -= val
+        self._old = val
+        self._len -= 1
