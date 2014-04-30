e2dddbc2dcf12f543e56150ea366144a1793d31f
Alebats <alebat25@yahoo.it>
2014-04-30 16:02:33 +0200

    Not today -.- fixed some problems but interpolation still does not work

--- pyHRV/indexes/SupportValues.py
+++ pyHRV/indexes/SupportValues.py
@@ -1,7 +1,6 @@
 __author__ = 'AleB'
 
 from BaseIndexes import SupportValue
-from SupportValuesCollection import SupportValuesCollection
 from pyHRV.utility import template_interpolation
 from pyHRV.PyHRVSettings import PyHRVDefaultSettings as Ps
 
@@ -129,29 +128,28 @@ class InterpolationSV(SupportValue):
     """Support value: INTERPOLATION of the VALUES in (seconds O bpm) [~ xOy]
     """
 
-    def __init__(self, sv_collection):
+    def __init__(self):
         self._v = []
         self._lx = 0
         self._ly = None
         self._fx = 0
         self._fy = None
-        assert isinstance(sv_collection, SupportValuesCollection)
-        self._sv = sv_collection
 
     def enqueuing(self, new_value):
-        y = 60000 / new_value
-        x = self._lx + new_value / 1000  # NO TIME-TOLERANCE ERRORS!
+        y = 60000.0 / new_value
+        x = self._lx + new_value / 1000.0  # NO TIME-TOLERANCE ERRORS!
         if self._ly is None:  # with the first IBI I get the hr of the first (t=x=0) beat and of the second (t=x=IBI)
             self._fy = self._ly = y
-            self._fx = self._lx = 0  # NO TIME-TOLERANCE ERRORS!
+            self._fx = self._lx = 0.0  # NO TIME-TOLERANCE ERRORS!
         hr, t = template_interpolation([self._ly, y],
                                        [self._lx, x],
-                                       Ps.DefaultInterpolation.online_step_milliseconds / 1000.0)
+                                       Ps.default_interpolation_freq)
+        print ([self._ly, y], [self._lx, x]), hr
         self._v.extend(hr)
         self._lx = x  # NO TIME-TOLERANCE ERRORS!
 
     def dequeuing(self, old_value):
-        mt = old_value / 1000  # NO TIME-TOLERANCE ERRORS!
+        mt = old_value / 1000.0  # NO TIME-TOLERANCE ERRORS!
         ct = self._fx
         while ct + self._v[0] <= mt:
             ct += self._v[0]
