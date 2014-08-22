63c87c3357861b3b1767217ffa59aeeae1e980d3
Alebats <alebat25@yahoo.it>
2014-08-22 15:45:17 +0200

    GenerateData

--- pyHRV/Snippets/Example.py
+++ pyHRV/Snippets/Example.py
@@ -1,21 +1,7 @@
 __author__ = 'AleB'
 
 import pyHRV
-import numpy
 import pandas
-
-# We create as example a random database in the file "RD.txt"
-ls = numpy.random.randint(50, 80, 100)
-ln = ["Red", "Green", "Blue", "Relaxed", "Noise"]
-ll = []
-# noinspection PyTypeChecker
-for s in ls:
-    # noinspection PyArgumentList
-    l = ln[int(numpy.random.rand() * len(ln))]
-    for i in xrange(int(s)):
-        ll.append(l)
-rr = numpy.random.randint(500, 1500, len(ll))
-pandas.DataFrame({"IBI": rr, "label": ll}).to_csv("RD.txt", sep="\t", index=False, header=True)
 
 # We load the data series from a csv file with tabulations as separators
 # IBI from the column "IBI"
